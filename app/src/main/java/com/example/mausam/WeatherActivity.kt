47493780022e49a2eebd0430cf553235bcc8618f
shreya <shreyagarg06031412@gmail.com>
2024-11-27 19:52:46 +0530

    sec

--- app/src/main/java/com/example/mausam/WeatherActivity.kt
+++ app/src/main/java/com/example/mausam/WeatherActivity.kt
@@ -1,28 +1,20 @@
 package com.example.mausam
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class WeatherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_weather)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val start = findViewById<Button>(R.id.start)
-        start.setOnClickListener{
-            val intent = Intent(this, WeatherActivity::class.java)
-            startActivity(intent)
         }
     }
 }
